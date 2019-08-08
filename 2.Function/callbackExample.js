748cf6eeb31f35dd272f2e5872aa87492ac0da1a
Min Yeong <pmy0792@naver.com>
2019-08-08 16:01:39 +0900

    assignment 2

--- 2.Function/callbackExample.js
+++ 2.Function/callbackExample.js
@@ -1,10 +1,12 @@
 const list=[1,2,3,4,5,6,7,8,9,10];
 
 const callbackExample=(items,callback)=>{
+    setTimeout(function(){
         const sum = items.reduce((a,b)=>(a+b));
         callback(sum);
-    };
-        
-const func=result=>console.log(result);
-callbackExample(list,func);
-//기존에 setTimeout함수는 왜있던거죠??ㅠㅠ
+    },0);
+};        
+callbackExample(list,function(result){
+    console.log(result);
+});
+console.log('first');
