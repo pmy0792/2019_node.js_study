2c3a6116b99e9182d833221f25cadebddeea0cfc
Min Yeong <pmy0792@naver.com>
2019-08-10 15:21:05 +0900

    assignment 2

--- 2.Function/callbackExample.js
+++ 2.Function/callbackExample.js
@@ -1,12 +1,12 @@
 const list=[1,2,3,4,5,6,7,8,9,10];
 
 const callbackExample=(items,callback)=>{
-    setTimeout(function(){
+    setTimeout(()=>{
         const sum = items.reduce((a,b)=>(a+b));
         callback(sum);
     },0);
 };        
-callbackExample(list,function(result){
+callbackExample(list,(result)=>{
     console.log(result);
 });
 console.log('first');
