12aa22c52684f1ec7e67001f3bb36d24ee8caf7a
Min Yeong <pmy0792@naver.com>
2019-08-11 16:12:28 +0900

    assignment 4

---
+++ 4.Modules/calculateByFile.js
@@ -0,0 +1,38 @@
+const calc=require('./opr');
+const add=calc.add;
+const subtract=calc.subtract;
+const multiply=calc.multiply;
+const divide=calc.divide;
+
+const fs=require('fs');
+let input="";
+fs.readFile('input.txt',(err,data)=>{
+    if (err)
+        throw err;
+    
+    const input=data.toString();
+
+    const split_input=input.split(',');
+
+const fir =parseFloat((split_input)[0]);
+const oper =(split_input)[1];
+const sec =parseFloat((split_input)[2]);
+
+let result;
+
+if (oper == '+')
+    result = add(fir,sec);
+if (oper == '-')
+    result = subtract(fir,sec);
+if (oper == '*')
+    result = multiply(fir,sec);
+if (oper == '/')
+    result = divide(fir,sec);
+
+
+const content = result;
+fs.writeFile('output.txt',content,(err)=>{
+    if (err)
+        throw err;
+});
+});
