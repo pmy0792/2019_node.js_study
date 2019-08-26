8c39ad4c9ffd9d8d5150f6ee4f0c29acd17df9ef
Min Yeong <pmy0792@naver.com>
2019-08-26 19:58:33 +0900

    assignment 6

--- 6.Middlewares/app.js
+++ 6.Middlewares/app.js
@@ -1,16 +1,12 @@
 const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
-const session=require('express-session');
 
 const users = [];
 app.use(express.static(__dirname+'/'));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(session({
-    secret:'keyboard cat',
-    resave:false,
-    saveUnintialized:true
-}));
+
+
 
 app.get('/',(req,res)=>{
     res.redirect('/signup.html')
@@ -19,28 +15,15 @@ app.get('/',(req,res)=>{
 app.post('/signup',(req,res)=>{
     const userId =req.body.userId;
     const password =req.body.password;
-    const name=req.body.name;
-    const department=req.body.department;
-    const emailaddress=req.body.emailaddress;
-    const phonenumber=req.body.phonenumber;
+
     const idx_id=users.findIndex((item,idx)=>{
         return item.Id ===userId});
-    const idx_name=users.findIndex((item,idx)=>{
-        return item.name ===name});
-    const idx_department=users.findIndex((item,idx)=>{
-        return item.department===department});
-    const idx_email=users.findIndex((item,idx)=>{
-        return item.emailaddress=emailaddress});
-    const idx_phone=users.findIndex((item,idx)=>{
-        return item.phonenumber===phonenumber});
 
-        if ((idx_id>-1)||(idx_department>-1)||(idx_email)>-1
-        ||(idx_name>-1)||(idx_phone>-1)){
+
+        if (idx_id>-1){
             res.send('User already exists');    
         }
-        else {users.push({Id:userId,PW:password,NAME:name,
-            DEPARTMENT:department,EMAILADDRESS:emailaddress,
-            PHONENUMBER:phonenumber});
+        else {users.push({Id:userId,PW:password});
         console.log(users);
         res.redirect('/login.html');
 }});
@@ -60,14 +43,11 @@ app.post('/login',(req,res)=>{
         if((idx_id>-1)&&idx_pw==-1){
             res.send('Password Wrong')
         }
-        else{
-            req.session.userId=userId;
-            res.redirect('/profile')};
+        if (((idx_id>-1)&&(idx_pw>-1))){
+            res.send("Welcome "+userId+"!")};
 })
     
-app.get('/profile',(req,res)=>{
-    console.log('zz');
-})
+
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
